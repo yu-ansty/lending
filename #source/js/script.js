b80754e4d046b94bf55dd196be6307bac871a885
yu-ansty <anastasiakorovina17@gmail.com>
2020-09-28 15:47:29 +0300

    add slider

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,3 +1,5 @@
+@@include('slick.min.js')
+
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
         $('.header__burger,.header-top__navigation').toggleClass('active');
@@ -6,5 +8,26 @@ $(document).ready(function () {
     $('.header-top__body').click(function (event) {
         $('.header-top__navigation').removeClass('active');
         $('body').removeClass('lock');
+    });
+    $('.container-one').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 490,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
 });
